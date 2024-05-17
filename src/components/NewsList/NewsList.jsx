e8d8c97c3f9a87cd30c46ab94fb85fef554a7a4e
YuliiaGagina <ygaginazy@gmail.com>
2024-05-18 00:00:09 +0300

    made refactor for main

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -1,14 +1,16 @@
 import styles from "./styles.module.css";
 import NewsItem from "./../NewsItem/NewsItem";
+import withSkeleton from "./../helpers/hocs/withskeleton";
 
 const NewsList = ({ news }) => {
   return (
     <ul className={styles.list}>
-      {news.map((item) => {
+      {news?.map((item) => {
         return <NewsItem item={item} key={item.id} />;
       })}
     </ul>
   );
 };
 
-export default NewsList;
+const NewsListWithSkeleton = withSkeleton(NewsList, "item", 10);
+export default NewsListWithSkeleton;
