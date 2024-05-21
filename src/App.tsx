36b0af88c87a85433052a1770e6ff0c7cb37e707
YuliiaGagina <ygaginazy@gmail.com>
2024-05-21 14:59:30 +0300

    create context

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
+
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import { useTheme } from "./context/ThemeContext";
 import Main from "./pages/Main/Main";
 
 
+
+
+
+
 function App() {
+  const {isDark} = useTheme()
+
   return (
-    <>
+
+    <div className={`app ${isDark ? 'dark' : 'light'} `}>
       <Header />
       <div className="container">
-        <Main />
+        <Main  />
       </div>
       <Footer />
-    </>
+      </div>
+    
+     
   );
 }
 
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,11 +3,13 @@ import styles from "./styles.module.css";
 import LatestNews from "../../components/LatestNews/LatestNews";
 import NewsByFilter from "../../components/NewsByFilter/NewsByFilter";
 
+
+
 const Main = () => {
   return (
     <main className={styles.main}>
       <LatestNews />
-      <NewsByFilter />
+      <NewsByFilter  />
     </main>
   );
 };
